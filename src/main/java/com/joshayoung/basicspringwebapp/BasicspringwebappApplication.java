8893d3f485b10c41f5c13f2cd7ecd0b5def57fd3
Josh Young <j22cal@gmail.com>
2021-03-01 11:59:22 -0600

    initial commit

---
+++ src/main/java/com/joshayoung/basicspringwebapp/BasicspringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.joshayoung.basicspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicspringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicspringwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshayoung/basicspringwebapp/BasicspringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshayoung.basicspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicspringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
